0864bba1e4540a9c3589473045c2294dd2fcd45e
Lisa <0isamatos0@gmail.com>
2025-06-05 10:05:02 -0300

    Arrumar Cartao e Flor para como estava no outro código

--- src/Model/Flor.java
+++ src/Model/Flor.java
@@ -1,4 +1,32 @@
 package Model;
 
 public abstract class Flor {
+    private String cor;
+    private int precoUnidade;
+    private enum tamanho{
+        PEQUENO,
+        MEDIO,
+        GRANDE,
+        CONJUNTO; //Como Flor de Cera e Hoya Bella
+    }
+
+    public Flor(String cor, int precoUnidade) {
+        this.cor = cor;
+        this.precoUnidade = precoUnidade;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getPrecoUnidade() {
+        return precoUnidade;
+    }
+    public void setPrecoUnidade(int precoUnidade) {
+        this.precoUnidade = precoUnidade;
+    }
 }
