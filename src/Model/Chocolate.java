5458bb47d3931d1b4bb9abcf28e5d3b4e32f9bba
Johns-300605 <joaohzr30@gmail.com>
2025-06-06 09:23:49 -0300

    Chocolate e pelucia

--- src/Model/Chocolate.java
+++ src/Model/Chocolate.java
@@ -1,4 +1,37 @@
 package Model;
 
 public class Chocolate {
+    public String sabor;
+    public float preco;
+    public String marca;
+
+    public Chocolate(String nome, String tipo, float preco, String marca) {
+        this.sabor = sabor;
+        this.preco = preco;
+        this.marca = marca;
+    }
+
+    public String getSabor() {
+        return sabor;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setSabor(String sabor) {
+        this.sabor = sabor;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
 }
--- src/Model/Pelucia.java
+++ src/Model/Pelucia.java
@@ -1,4 +1,37 @@
 package Model;
 
 public class Pelucia {
+    private String nome;
+    private String tipo;
+    public float preco;
+
+    public Pelucia(String nome, String tipo, float preco) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
 }
